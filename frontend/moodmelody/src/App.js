29bf48fb0b844b015b1c346fd0688dc82e49a738
Krishiv Gubba <kgubba@wisc.edu>
2025-05-13 01:36:35 +0530

    typescript stuff?

--- frontend/moodmelody/src/App.js
+++ frontend/moodmelody/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SpotifyAuth from './components/SpotifyAuth';
-import StartScreen from './components/StartScreen';
+import SpotifyAuth from './components/SpotifyAuth.tsx';
+import StartScreen from './components/StartScreen.tsx';
 import './App.css';
 
 function App() {
